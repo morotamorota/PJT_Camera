6fa027b92af7a262da9d0d1d8c0d2ef3216a25e2
koichi <2020m086@kuas.ac.jp>
2023-10-04 13:15:21 +0900

    Initial Commit

---
+++ PJT_Camera/PJT_CameraApp.swift
@@ -0,0 +1,17 @@
+//
+//  PJT_CameraApp.swift
+//  PJT_Camera
+//
+//  Created by 諸田紘一 on 2023-10-04.
+//
+
+import SwiftUI
+
+@main
+struct PJT_CameraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
